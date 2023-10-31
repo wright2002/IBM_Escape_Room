249cc747bc284b2cb2ea2ce12f580e2838f78d27
wright2002 <70294686+wright2002@users.noreply.github.com>
2023-10-30 23:50:12 -0400

    skeleton of second puzzle and style sheets diversified for individual pages

---
+++ digit_script.js
@@ -0,0 +1,19 @@
+
+function setUp() {
+    
+}
+
+const isCorrect = (solution, content) => {
+    
+}
+
+
+const showModal = () => {
+    document.getElementById('message').innerText = "You Won!";
+    document.getElementById('modal').classList.remove("hide");
+
+}
+
+const hideModal = () => {
+    document.getElementById('modal').classList.add("hide");
+}
