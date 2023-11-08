87f83c60ca57fbd4e91d207afc079440a045c8fe
wright2002 <70294686+wright2002@users.noreply.github.com>
2023-11-08 04:33:12 -0500

    puzzle 2 operational, randomization not implemented yet

--- digit_script.js
+++ digit_script.js
@@ -2,9 +2,27 @@ const value1 = 7;
 const value2 = 1;
 const value3 = 4;
 
+let ul = document.querySelectorAll('li');
+const tiles= ["1", "2", "3", "", "", "", 
+			"", "", "", "", "", "", 
+			"", "", "", "", "", "",
+			"", "", "", "", "", "", 
+			"", "", "", "", "", "", 
+			"", "", "", "", "", ""]
+
 
 function setUp() {
-    
+    fillGrid(ul, tiles);
+
+}
+
+const showModal = () => {
+    document.getElementById('message').innerText = "You Won!";
+    document.getElementById('modal').classList.remove("hide");
+}
+
+const hideModal = () => {
+    document.getElementById('modal').classList.add("hide");
 }
 
 const checkPasskey = () => {
@@ -20,11 +38,26 @@ const checkPasskey = () => {
 	}
 }
 
-const showModal = () => {
-    document.getElementById('message').innerText = "You Won!";
-    document.getElementById('modal').classList.remove("hide");
+const fillGrid = (items, tiles) => {
+    let shuffled = shuffle(tiles);
+
+    items.forEach((item, i) => {
+        item.innerText = shuffled[i];
+    })
 }
 
-const hideModal = () => {
-    document.getElementById('modal').classList.add("hide");
-}
+fillGrid(ul, tiles);
+
+const shuffle = (arr) => {
+    const copy = [...arr];
+    // loop over the array
+    for(let i = 0; i < copy.length; i++) {
+        // for each index,i pick a random index j 
+        let j = parseInt(Math.random()*copy.length);
+        // swap elements at i and j
+        let temp = copy[i];
+        copy[i] = copy[j];
+        copy[j] = temp;
+    }   
+    return copy;
+ }
